8c8f6908112a1926f94cd399d5382493141b9656
Action2thefuture <action2thefuture@gmail.com>
2024-07-04 13:46:41 +0200

    Add : taking env var from input of heredoc

--- src/exec/redir.c
+++ src/exec/redir.c
@@ -6,7 +6,7 @@
 /*   By: junsan <junsan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 12:01:59 by junsan            #+#    #+#             */
-/*   Updated: 2024/07/03 19:06:08 by junsan           ###   ########.fr       */
+/*   Updated: 2024/07/04 13:22:49 by junsan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ static int	here_doc_redir(t_ast *node, t_info *info)
 	if (dup2(info->origin_stdout_fd, STDOUT_FILENO) == -1)
 		return (fd_log_error("Dup origin_stdout_fd error!", NULL, NULL));
 	info->stdin_fd = open_file_with_mode(HEREDOC_TMP, WRITE);
-	here_doc(info->stdin_fd, node->data);
+	here_doc(info->stdin_fd, node->data, info);
 	if (dup2(info->tmp_fd, STDOUT_FILENO) == -1)
 		return (fd_log_error("Dup error!", NULL, NULL));
 	return (SUCCESS);
