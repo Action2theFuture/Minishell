983715d23f070612e4dff2e85a769edb94b92758
Action2thefuture <action2thefuture@gmail.com>
2024-07-29 11:12:04 +0200

    Add : direction in print_tree

--- src/exec/subshell.c
+++ src/exec/subshell.c
@@ -6,7 +6,7 @@
 /*   By: junsan <junsan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/27 19:38:46 by junsan            #+#    #+#             */
-/*   Updated: 2024/07/28 22:10:15 by junsan           ###   ########.fr       */
+/*   Updated: 2024/07/29 11:11:29 by junsan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,7 @@ static void	traverse_tree_in_subshell(t_ast **node, t_info *info)
 				last_node = last_node->left;
 			if (last_node->data[0] == ')')
 				traverse_tree_in_subshell(&last_node, info);
+		}
 */
 static void	process_logical_node_in_subshell(t_ast **node, t_info *info)
 {
