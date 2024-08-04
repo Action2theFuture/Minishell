b3b174bb3442c299bf9ff5214a771021e742c65d
Action2thefuture <action2thefuture@gmail.com>
2024-08-04 18:19:08 +0200

    Add : redirect to null in process phrase node

--- src/exec/process_execute.c
+++ src/exec/process_execute.c
@@ -6,7 +6,7 @@
 /*   By: junsan <junsan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 22:12:12 by junsan            #+#    #+#             */
-/*   Updated: 2024/08/04 14:44:51 by junsan           ###   ########.fr       */
+/*   Updated: 2024/08/04 18:17:15 by junsan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,4 +55,6 @@ void	process_phrase_node(t_ast *node, t_info *info)
 	if (cmd_node && info->status == SUCCESS)
 		info->exit_status = dispatch_cmd(cmd_node, info);
 	(free_args(info->redir_args), info->redir_args = NULL, restore_fds(info));
+	if (redir_node)
+		redirect_output_to_null();
 }
