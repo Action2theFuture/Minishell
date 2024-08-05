74551405a67738b3fb0d2a009056cbe3ec51f890
Action2thefuture <action2thefuture@gmail.com>
2024-08-05 21:03:54 +0200

    Fix: still memory leak with subshell

--- src/exec/subshell.c
+++ src/exec/subshell.c
@@ -6,50 +6,51 @@
 /*   By: junsan <junsan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/27 19:38:46 by junsan            #+#    #+#             */
-/*   Updated: 2024/08/04 15:32:24 by junsan           ###   ########.fr       */
+/*   Updated: 2024/08/05 21:02:54 by junsan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	process_logical_node_in_subshell(t_ast **node, t_info *info);
+static void	process_logical_node_in_subshell(t_ast *node, t_info *info);
+static void	traverse_tree_in_subshell(t_ast *node, t_info *info);
 
-static void	handle_special_nodes(t_ast	**node, t_info *info)
+static void	handle_special_nodes(t_ast	*node, t_info *info)
 {
 	t_info	subshell_info;
 
-	if ((*node)->type == LOGICAL)
+	if (node->type == LOGICAL)
 		process_logical_node_in_subshell(node, info);
-	else if ((*node)->type == PIPE)
-		process_pipe_node(*node, info);
-	else if ((*node)->type == SUBSHELL)
+	else if (node->type == PIPE)
+		process_pipe_node(node, info);
+	else if (node->type == SUBSHELL)
 	{
-		init_info(&subshell_info, info->env, *node);
-		process_logical_node_in_subshell(&(*node)->left, &subshell_info);
+		init_info(&subshell_info, info->env, node);
+		traverse_tree_in_subshell(node->left, &subshell_info);
 		info->exit_status = subshell_info.exit_status;
 		clear_info(&subshell_info);
 	}
 }
 
-static void	traverse_tree_in_subshell(t_ast **node, t_info *info)
+static void	traverse_tree_in_subshell(t_ast *node, t_info *info)
 {
-	if (*node == NULL)
+	if (node == NULL)
 		return ;
-	if ((*node)->type == LOGICAL || (*node)->type == PIPE || \
-		(*node)->type == SUBSHELL)
+	if (node->type == LOGICAL || node->type == PIPE || \
+		node->type == SUBSHELL)
 		handle_special_nodes(node, info);
-	else if ((*node)->type != PIPE && (*node)->type != LOGICAL)
+	else if (node->type != PIPE && node->type != LOGICAL)
 	{
-		if ((*node)->type == PHRASE && info->status == SUCCESS)
+		if (node->type == PHRASE && info->status == SUCCESS)
 		{
-			process_phrase_node(*node, info);
-			if (!(*node)->left)
+			process_phrase_node(node, info);
+			if (!node->left)
 				return ;
 		}
-		if ((*node)->left)
-			traverse_tree_in_subshell(&(*node)->left, info);
-		if ((*node)->right)
-			traverse_tree_in_subshell(&(*node)->right, info);
+		if (node->left)
+			traverse_tree_in_subshell(node->left, info);
+		if (node->right)
+			traverse_tree_in_subshell(node->right, info);
 	}
 }
 
@@ -66,18 +67,18 @@ static void	traverse_tree_in_subshell(t_ast **node, t_info *info)
 				traverse_tree_in_subshell(&last_node, info);
 		}
 */
-static void	process_logical_node_in_subshell(t_ast **node, t_info *info)
+static void	process_logical_node_in_subshell(t_ast *node, t_info *info)
 {
 	int		status;
 
-	traverse_tree_in_subshell(&(*node)->right, info);
+	traverse_tree_in_subshell(node->right, info);
 	status = info->exit_status;
-	if (ft_strncmp((*node)->data, "&&", 2) == 0 && status == SUCCESS)
-		traverse_tree_in_subshell(&(*node)->left, info);
-	else if (ft_strncmp((*node)->data, "||", 2) == 0)
+	if (ft_strncmp(node->data, "&&", 2) == 0 && status == SUCCESS)
+		traverse_tree_in_subshell(node->left, info);
+	else if (ft_strncmp(node->data, "||", 2) == 0)
 	{
 		if (info->exit_status > 0)
-			traverse_tree_in_subshell(&(*node)->left, info);
+			traverse_tree_in_subshell(node->left, info);
 	}
 }
 
@@ -95,7 +96,7 @@ int	process_subshell_node(t_ast *node, t_info *info)
 		if (backup_stdio(info) == FAILURE)
 			fd_log_error(NULL, NULL, strerror(errno));
 		set_signal_handler(DFL);
-		traverse_tree_in_subshell(&subshell_node, info);
+		traverse_tree_in_subshell(subshell_node, info);
 		if (restore_stdio(info) == FAILURE)
 			fd_log_error(NULL, NULL, strerror(errno));
 		cleanup_and_exit(info->exit_status, NULL, NULL, info);
